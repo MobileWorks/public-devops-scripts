5d3da3d0fcd5436779233d17b4de7c2f21fd7617
Daniel Bowd <daniel@leadgenius.com>
2016-07-14 13:23:09 -0700

    The case of the missing backslash

--- notify_deploys.sh
+++ notify_deploys.sh
@@ -45,7 +45,7 @@ if [ -n "$MAILGUN_API_KEY" ]; then
         -F from='Deployments <postmaster@leadgeni.us>' \
         -F to='product@leadgenius.com' \
         -F subject="Deployment: $CIRCLE_PROJECT_REPONAME" \
-        -F text="Your friendly neighorhood LG engineering team just deployed a new version."
+        -F text="Your friendly neighorhood LG engineering team just deployed a new version." \
         --form-string html="<p>Your friendly neighorhood LG engineering team just deployed a new version.</p><p>Changes since last deployment:<br>$CIRCLE_COMPARE_URL</p>"
     echo ""
 else
